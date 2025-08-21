f2416afb9f787ec0a5a5da0483b92aaa0fd95bdf
abhisekghosh018 <abhisekghosh018@gmail.com>
2025-08-21 18:06:22 +0530

    adds ReaderGroup table

---
+++ ReadOrbit.DOMAIN/DomainEntities/ReaderGroup.cs
@@ -0,0 +1,11 @@
+﻿namespace ReadOrbit.DOMAIN.DomainEntities
+{
+    public class ReaderGroup
+    {
+        public int Id { get; set; }
+        public string BookReaderId { get; set; }
+        public BookReader BookReader { get; set; }
+        public string BookId { get; set; }
+        public Book Book { get; set; }  
+    }
+}
