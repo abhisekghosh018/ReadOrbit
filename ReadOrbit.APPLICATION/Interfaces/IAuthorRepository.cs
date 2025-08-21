bbd27fd66ed67d4b6030f8f14dbb3427f0168340
abhisekghosh018 <abhisekghosh018@gmail.com>
2025-08-21 18:01:11 +0530

    fixes bugs

--- ReadOrbit.APPLICATION/Interfaces/IAuthorRepository.cs
+++ ReadOrbit.APPLICATION/Interfaces/IAuthorRepository.cs
@@ -1,9 +1,4 @@
 ﻿using ReadOrbit.DOMAIN.DomainEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ReadOrbit.APPLICATION.Interfaces
 {
